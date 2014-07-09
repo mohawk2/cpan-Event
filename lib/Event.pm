383fba80be56ca1feffc8071f199a3f1e727e875
Ed J <mohawk2@users.noreply.github.com>
2014-07-09 05:21:33 +0100

    Rely on Inline "with" hook error message.

--- lib/Event.pm
+++ lib/Event.pm
@@ -205,19 +205,13 @@ _load_watcher($_) for qw(idle io signal timer var);
 
 # Provide hints to Inline.pm for usage:
 # use Inline with => 'Event';
-
 sub Inline {
     my ($class, $language) = @_;
-    if ($language ne 'C') {
-	warn "Warning: Event.pm does not provide Inline hints for the $language language\n";
-	return;
-    }
-
+    return if $language ne 'C'; # Inline gives good error message
     require Event::MakeMaker;
     my $path = $Event::MakeMaker::installsitearch;
     require Config;
     my $so = $Config::Config{so};
-
     return {
 	INC => "-I $path/Event",
 	TYPEMAPS => "$path/Event/typemap",
