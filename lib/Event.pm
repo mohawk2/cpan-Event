2809e4aca132c2e37c57f6553d63019312ddf0da
Ed J <mohawk2@users.noreply.github.com>
2016-06-06 03:01:41 +0100

    1.25

--- lib/Event.pm
+++ lib/Event.pm
@@ -13,7 +13,7 @@ use base 'Exporter';
 use Carp;
 eval { require Carp::Heavy; };  # work around perl_call_pv bug XXX
 our $API;
-our $VERSION = '1.24';
+our $VERSION = '1.25';
 
 # If we inherit DynaLoader then we inherit AutoLoader; Bletch!
 require DynaLoader;
